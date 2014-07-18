64c6c72c593a0923d16a6702afdc414cbdccbba1
Matt Chu <matt.chu@gmail.com>
2014-07-18 10:40:50 -0700

    wip: new build, new sha1

--- Formula/apache-spark-kinesis.rb
+++ Formula/apache-spark-kinesis.rb
@@ -6,7 +6,7 @@ class ApacheSparkKinesis < Formula
   #url "http://d3kbcqa49mib13.cloudfront.net/spark-1.0.0-bin-hadoop2.tgz"
   url "http://thmttch.net/spark-1.1.0-SNAPSHOT-bin-hadoop2.4.0.tar.gz"
   version "1.1.0-SNAPSHOT"
-  sha1 "8d4cf824977e8923e859bef0640c1defd5245e64"
+  sha1 "940e65ebe65d90bf4a28b5416719b4fa86856940"
 
   def install
     rm_f Dir["bin/*.cmd"]
