8a4a4626ffdca7984eee0a8853d71bd86567e51e
Matt Chu <matt.chu@gmail.com>
2016-06-14 21:45:45 -0700

    wip: added notes for making it work

--- Formula/s3-backer-1.4.2.rb
+++ Formula/s3-backer-1.4.2.rb
@@ -3,17 +3,21 @@ class S3Backer142 < Formula
   desc 'FUSE-based single file backing store via Amazon S3'
   homepage 'https://github.com/archiecobbs/s3backer'
   url 'https://github.com/archiecobbs/s3backer/archive/1.4.2.tar.gz'
-  sha256 '7a6f028d16301b0fd32679eebe34ff2d614f343979ee7a0fab0110b85934aaca'
+  sha256 '0cbfd943067f5a6ea8d8883695270ddecb33c82c3150e873769ed5a8c45d1f93'
 
   depends_on 'pkg-config' => :build
   depends_on 'openssl'
-  depends_on :osxfuse
+  depends_on 'osxfuse'
 
   def install
+    system './autogen.sh'
     inreplace 'configure', '-lfuse', '-losxfuse'
 
-    system './configure', "--prefix=#{prefix}"
+    # TODO: had to do `brew link openssl --force`, and then drop the FLAGS, and it works
+    system 'LDFLAGS=/usr/local/opt/openssl/lib', 'CPPFLAGS=/usr/local/opt/openssl/include', './configure', "--prefix=#{prefix}"
     system 'make', 'install'
+
+    # TODO: now need to install it to the right place
   end
 
   test do
